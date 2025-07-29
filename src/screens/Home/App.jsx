e1ee32f493e12b5785712120e4ed51dc37238ea4
BA\bilal.naveed <Bilalnaveed887@gmail.com>
2025-07-29 16:05:49 +0500

    Services Page conflicts

--- src/screens/Home/App.jsx
+++ src/screens/Home/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import FrameWrapperByAnima from "./sections/FrameWrapperByAnima"; // Your existing navigation component
 import { Service } from "./Service";
-import { Home } from "./Home";
 import { FrameWrapperByAnima } from "./sections/FrameWrapperByAnima";
+import { Home } from "./Home";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
      <FrameWrapperByAnima/>
       <Switch>
         {/* Other routes */}
-        {/* <Route exact path="/" component={Home} /> */}
+        { <Route exact path="/" component={Home} /> }
 
         <Route exact path="/services" component={Service} />
       </Switch>
